55a882962cebbb088d139321fdda1ac091649eb8
o3-ozone <85118430+o3-ozone@users.noreply.github.com>
2023-06-13 22:35:20 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import time
 from module import WIDTH,HEIGHT,SQUARESIZE,board,BLACK,WHITE
 from module.game import Game
-from module.piece import piece
-
+from module.piece import Piece
+from minimax.algorithm import minimax
 FPS=60
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Draughts")
@@ -17,26 +18,27 @@ def main():
     run = True
     clock = pygame.time.Clock()
     games = Game(WIN)
+
     while run:
         clock.tick(FPS)
         
         if games.winner() != None:
             print(games.winner())
+        
+        if games.turn == BLACK :
+            value, new_board = minimax(games.get_board(), 3, BLACK, games)
+            print(value)
+            games.ai_move(new_board)
 
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 run = False
             if events.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                print(games.turn)
                 if games.turn == WHITE:
-                        row , col = get_pos_from_mouse(pos)
-                        games.select(WHITE,row,col)
-
-                if games.turn == BLACK:
-                        row , col = get_pos_from_mouse(pos)
-                        games.select(BLACK,row,col)
-
+                    row , col = get_pos_from_mouse(pos)
+                    games.select(row,col)
+                
         games.update()
 
     pygame.quit()
